c3c1f4dc342ec6ac643590383a9a36fc5ded857b
frank-cn <frank-cn-asia@hotmail.com>
2023-09-12 16:08:52 +0800

    ce loss

--- mmyolo/models/losses/__init__.py
+++ mmyolo/models/losses/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .iou_loss import IoULoss, bbox_overlaps
 from .oks_loss import OksLoss
+from .ce_loss import CrossEntropyLoss
 
-__all__ = ['IoULoss', 'bbox_overlaps', 'OksLoss']
+__all__ = ['IoULoss', 'bbox_overlaps', 'OksLoss', 'CrossEntropyLoss']
