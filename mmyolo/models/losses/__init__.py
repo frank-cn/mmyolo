ec5a976347c8fc3ccbdb25503da746c2f87ae461
frank-cn <frank-cn-asia@hotmail.com>
2023-10-17 11:07:57 +0800

    test

--- mmyolo/models/losses/__init__.py
+++ mmyolo/models/losses/__init__.py
@@ -2,5 +2,6 @@
 from .iou_loss import IoULoss, bbox_overlaps
 from .oks_loss import OksLoss
 from .ce_loss import CrossEntropyLoss
+from .gvf_loss import QualityVarialFocalLoss
 
-__all__ = ['IoULoss', 'bbox_overlaps', 'OksLoss', 'CrossEntropyLoss']
+__all__ = ['IoULoss', 'bbox_overlaps', 'OksLoss', 'CrossEntropyLoss', 'QualityVarialFocalLoss']
