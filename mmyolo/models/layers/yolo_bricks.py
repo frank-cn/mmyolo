4388429f99d638b33e7a519e5efd6e8cfe62e5bf
frank-cn <frank-cn-asia@hotmail.com>
2024-05-16 10:02:39 +0800

    Update

--- mmyolo/models/layers/yolo_bricks.py
+++ mmyolo/models/layers/yolo_bricks.py
@@ -11,10 +11,12 @@ from mmdet.models.layers.csp_layer import \
 from mmdet.utils import ConfigType, OptConfigType, OptMultiConfig
 from mmengine import MessageHub
 from mmengine.model import BaseModule
+from mmengine.runner import EpochBasedTrainLoop
 from mmengine.utils import digit_version
 from torch import Tensor
 
 from mmyolo.registry import MODELS
+from mmyolo.registry import LOOPS
 
 if digit_version(torch.__version__) >= digit_version('1.7.0'):
     MODELS.register_module(module=nn.SiLU, name='SiLU')
@@ -2692,124 +2694,18 @@ class CSPLayerZF(BaseModule):
         return self.final_conv(x_final)
 
 
-import torch.nn.functional as F
-from mmdet.models.losses.utils import weighted_loss
-@weighted_loss
-def quality_focal_loss(pred, target, beta=2.0):
-    r"""Quality Focal Loss (QFL) is from `Generalized Focal Loss: Learning
-    Qualified and Distributed Bounding Boxes for Dense Object Detection
-    <https://arxiv.org/abs/2006.04388>`_.
+@LOOPS.register_module()
+class ZFEpochBasedTrainLoop(EpochBasedTrainLoop):
 
-    Args:
-        pred (torch.Tensor): Predicted joint representation of classification
-            and quality (IoU) estimation with shape (N, C), C is the number of
-            classes.
-        target (tuple([torch.Tensor])): Target category label with shape (N,)
-            and target quality label with shape (N,).
-        beta (float): The beta parameter for calculating the modulating factor.
-            Defaults to 2.0.
+    def run_epoch(self) -> None:
+        """Iterate one epoch."""
+        self.runner.call_hook('before_train_epoch')
+        self.runner.model.train()
+        for idx, data_batch in enumerate(self.dataloader):
+            self.run_iter(idx, data_batch)
+            self.run_iter(idx, data_batch)
 
-    Returns:
-        torch.Tensor: Loss tensor with shape (N,).
-    """
-    assert len(target) == 2, """target for QFL must be a tuple of two elements,
-        including category label and quality label, respectively"""
-    # label denotes the category id, score denotes the quality score
-    label, score = target
-
-    # negatives are supervised by 0 quality score
-    pred_sigmoid = pred.sigmoid()
-    scale_factor = pred_sigmoid
-    zerolabel = scale_factor.new_zeros(pred.shape)
-    loss = F.binary_cross_entropy_with_logits(
-        pred, zerolabel, reduction='none') * scale_factor.pow(beta)
-
-    # FG cat_id: [0, num_classes -1], BG cat_id: num_classes
-    bg_class_ind = pred.size(1)
-    pos = ((label >= 0) & (label < bg_class_ind)).nonzero().squeeze(1)
-    pos_label = label[pos].long()
-    # positives are supervised by bbox quality (IoU) score
-    scale_factor = score[pos] - pred_sigmoid[pos, pos_label]
-    loss[pos, pos_label] = F.binary_cross_entropy_with_logits(
-        pred[pos, pos_label], score[pos],
-        reduction='none') * scale_factor.abs().pow(beta)
-    return loss
+        self.runner.call_hook('after_train_epoch')
+        self._epoch += 1
 
 
-@MODELS.register_module()
-class QualityFocalLoss(nn.Module):
-    r"""Quality Focal Loss (QFL) is a variant of `Generalized Focal Loss:
-    Learning Qualified and Distributed Bounding Boxes for Dense Object
-    Detection <https://arxiv.org/abs/2006.04388>`_.
-
-    Args:
-        use_sigmoid (bool): Whether sigmoid operation is conducted in QFL.
-            Defaults to True.
-        beta (float): The beta parameter for calculating the modulating factor.
-            Defaults to 2.0.
-        reduction (str): Options are "none", "mean" and "sum".
-        loss_weight (float): Loss weight of current loss.
-        activated (bool, optional): Whether the input is activated.
-            If True, it means the input has been activated and can be
-            treated as probabilities. Else, it should be treated as logits.
-            Defaults to False.
-    """
-
-    def __init__(self,
-                 use_sigmoid=True,
-                 beta=2.0,
-                 reduction='mean',
-                 loss_weight=1.0):
-        super(QualityFocalLoss, self).__init__()
-        assert use_sigmoid is True, 'Only sigmoid in QFL supported now.'
-        self.use_sigmoid = use_sigmoid
-        self.beta = beta
-        self.reduction = reduction
-        self.loss_weight = loss_weight
-
-    def forward(self,
-                pred,
-                target,
-                weight=None,
-                avg_factor=None,
-                reduction_override=None):
-        """Forward function.
-
-        Args:
-            pred (torch.Tensor): Predicted joint representation of
-                classification and quality (IoU) estimation with shape (N, C),
-                C is the number of classes.
-            target (Union(tuple([torch.Tensor]),Torch.Tensor)): The type is
-                tuple, it should be included Target category label with
-                shape (N,) and target quality label with shape (N,).The type
-                is torch.Tensor, the target should be one-hot form with
-                soft weights.
-            weight (torch.Tensor, optional): The weight of loss for each
-                prediction. Defaults to None.
-            avg_factor (int, optional): Average factor that is used to average
-                the loss. Defaults to None.
-            reduction_override (str, optional): The reduction method used to
-                override the original reduction method of the loss.
-                Defaults to None.
-        """
-        assert reduction_override in (None, 'none', 'mean', 'sum')
-        reduction = (
-            reduction_override if reduction_override else self.reduction)
-        if self.use_sigmoid:
-            calculate_loss_func = quality_focal_loss
-            if isinstance(target, torch.Tensor):
-                # the target shape with (N,C) or (N,C,...), which means
-                # the target is one-hot form with soft weights.
-                calculate_loss_func = partial(
-                    quality_focal_loss_tensor_target, activated=self.activated)
-
-            loss_cls = self.loss_weight * calculate_loss_func(
-                pred,
-                target,
-                weight,
-                beta=self.beta,
-                reduction=reduction,
-                avg_factor=avg_factor)
-        else:
-            raise NotImplementedError
-        return loss_cls
